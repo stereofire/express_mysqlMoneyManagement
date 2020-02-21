919a69b24f38b9d8fd210ee988c56c28d7b8cf4b
stereofire <1243830635@qq.com>
2020-02-21 17:11:15 +0800

    有毒啊

---
+++ public/javascripts/支付方式.js
@@ -0,0 +1,25 @@
+function changePay(index){
+    switch(index){
+    case 1:
+    document.getElementById("radio-1").style.display = "";
+    document.getElementById("radio-2").style.display = "none";
+    document.getElementById("银联").style.display = "none";
+    break;
+    case 2:
+    document.getElementById("radio-1").style.display = "none";
+    document.getElementById("radio-2").style.display = "";
+    document.getElementById("银联").style.display = "none";
+    break;
+    case 3:
+    document.getElementById("radio-1").style.display = "none";
+    document.getElementById("radio-2").style.display = "none";
+    document.getElementById("银联").style.display = "";
+    break;
+    }
+}
+function changePay(index){
+    switch(index){
+        case 1:
+        alert("")
+    }
+}
